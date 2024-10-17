b2682b4015c369e202670f347b7a7fc271d0b715
Hengchen Yuan <66239555+Yhcrown@users.noreply.github.com>
2024-10-17 16:07:55 -0500

    Update process.py

--- process.py
+++ process.py
@@ -2,8 +2,23 @@ import pandas as pd
 def read_idoft():
     path = './pr-data.csv'
     df = pd.read_csv(path)
-    need_process_projects = df['Project URL'].loc[df['Category'] == 'UD' or df['Category'] == 'NOD' or df['Category'] == 'NIO' or df['Category'] == 'OSD']
+    need_process_projects = df[['Project URL', 'SHA Detected']].loc[
+        (df['Category'] == 'UD') |
+        (df['Category'] == 'NOD') |
+        (df['Category'] == 'NIO') |
+        (df['Category'] == 'OSD')
+        ].drop_duplicates(subset = ['Project URL'])
+    need_process_projects = need_process_projects.rename(columns={'SHA Detected': 'Project_Hash','Project URL':'Project_URL'})
+    need_process_projects['Project_Name'] = need_process_projects['Project_URL'].apply(lambda x : x.split('/')[-1])
     print(need_process_projects)
+
+def get_tests_to_focus(df):
+    need_process_projects = df[['Project URL', 'SHA Detected']].loc[
+        (df['Category'] == 'UD') |
+        (df['Category'] == 'NOD') |
+        (df['Category'] == 'NIO') |
+        (df['Category'] == 'OSD')
+        ].drop_duplicates(subset = ['Project URL',''])
 
 if __name__ == "__main__":
     read_idoft()
